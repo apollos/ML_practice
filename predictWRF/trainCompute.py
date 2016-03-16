2e4906a181285fe79bf4855070c4c4975d6b6ccd
Song Yu <apollos521@gmail.com>
2016-03-17 06:09:28 +0800

    change the loop

--- predictWRF/trainCompute.py
+++ predictWRF/trainCompute.py
@@ -17,60 +17,64 @@ if not trainDataList:
     exit(1)
 #Get test data, we need check together
 files = sF.listAllFiles("/home/yu/workspace/Data/test")
-
-#for i in range(0,2):#0 for task 0 and 1 for other tasks
-#The list is sperated to two part, [0] save all task 0 job information, task 1 save all other tasks
-#generate traing dataset
-#task 0 first
-xMat, yMatArray = myUtils.getMatfromList(trainDataList[0])
-
 testDataList = sF.shapeWrfComputingfile(files)
 if not testDataList:
     print("%s  %d: Get profiling information from %s failed." %(myDebug.file(), myDebug.line(), testFile))
     exit(1)
-xTMat, yTMatArray = myUtils.getMatfromList(testDataList[0])
+#for i in range(0,2):#0 for task 0 and 1 for other tasks
+#The list is sperated to two part, [0] save all task 0 job information, task 1 save all other tasks
+#generate traing dataset
+#task 0 first
 
-for yIdx in range(0, 2): #caculate computing result and then communication result
-    bestKList = []#we may meet sigular case while we predict, so we save 10 good k and try 10 times. [lowestError, bestK]
-    invalidKNum = 0
-    invalidKMin = inf
-    invalidKMax = 0.009
-    for k in arange(100, 0.009, -0.01):##find best k  
-    #for k in arange(27, 20, -1):##find best k  
-        yAssume = regression.lwlrTest(xTMat,xMat,yMatArray[yIdx].T,k)
-        if yAssume.all() == 0:
-            #print("%s  %d: regression.lwlr failed by k = %f." %(myDebug.file(), myDebug.line(), k))
-            invalidKNum += 1
-            if k > invalidKMax:
-                invalidKMax = k
-            if k < invalidKMin:
-                invalidKMin = k
-            continue
-            
-        rssE = regression.rssError(myUtils.transMat2List(yTMatArray[yIdx]), yAssume)
-        if len(bestKList) == 0:
-            bestKList.insert(0, [rssE, k])
-        else:
-            for idx in range(0, len(bestKList)):
-                if rssE < bestKList[idx][0]:
-                    bestKList.insert(idx, [rssE, k])
-                    if len(bestKList) > 10: #save 10 top k
-                        bestKList.pop()
-                    break
-    print bestKList
-    for kElement in bestKList:
-        taskPredict = 0
-        #get the result for cared data
-        xCheckMat = mat([myUtils.getConstantValue(), 200, 180])
-        wr = regression.lwlr(xCheckMat,xMat,yMatArray[yIdx].T,kElement[1])
-        if (wr != None):
-            #print wr
-            taskPredict = xCheckMat * wr
-            #print taskPredict
-            print "Result is %f with k %f" % (taskPredict, kElement[1])
-            break
-    #taskPredict = regression.lwlrTest(xCheckMat,xMat,yCommMat.T,bestK)
-    #print taskCoeff
+for taskIdx in range(0,2):
+    xMat, yMatArray = myUtils.getMatfromList(trainDataList[taskIdx])
+    xTMat, yTMatArray = myUtils.getMatfromList(testDataList[taskIdx])
+    print "Start compute taskIdx %d:" % taskIdx
+
+    for yIdx in range(0, 2): #caculate computing result and then communication result
+        bestKList = []#we may meet sigular case while we predict, so we save 10 good k and try 10 times. [lowestError, bestK]
+        invalidKNum = 0
+        invalidKMin = inf
+        invalidKMax = 0.009
+        aliveCount = 0
+        for k in arange(100, 0.009, -0.01):##find best k  
+        #for k in arange(27, 26, -2):##find best k  
+            if aliveCount % 1000 == 0:
+                print "I am alive!"
+            aliveCount += 1
+            yAssume = regression.lwlrTest(xTMat,xMat,yMatArray[yIdx].T,k)
+            if yAssume.all() == 0:
+                #print("%s  %d: regression.lwlr failed by k = %f." %(myDebug.file(), myDebug.line(), k))
+                invalidKNum += 1
+                if k > invalidKMax:
+                    invalidKMax = k
+                if k < invalidKMin:
+                    invalidKMin = k
+                continue
+            rssE = regression.rssError(myUtils.transMat2List(yTMatArray[yIdx]), yAssume)
+            if len(bestKList) == 0:
+                bestKList.insert(0, [rssE, k])
+            else:
+                for idx in range(0, len(bestKList)):
+                    if rssE < bestKList[idx][0]:
+                        bestKList.insert(idx, [rssE, k])
+                        if len(bestKList) > 10: #save 10 top k
+                            bestKList.pop()
+                        break
+        print bestKList
+        for kElement in bestKList:
+            taskPredict = 0
+            #get the result for cared data
+            xCheckMat = mat([myUtils.getConstantValue(), 200, 180])
+            wr = regression.lwlr(xCheckMat,xMat,yMatArray[yIdx].T,kElement[1])
+            if (wr != None):
+                #print wr
+                taskPredict = xCheckMat * wr
+                #print taskPredict
+                print "Result is %f with k %f" % (taskPredict, kElement[1])
+                break
+        #taskPredict = regression.lwlrTest(xCheckMat,xMat,yCommMat.T,bestK)
+        #print taskCoeff
 
 
 
